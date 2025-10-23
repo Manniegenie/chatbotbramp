9f40c9b150bd6797abe5947094713a567fc34eb0
Manniegenie <Manuelwurld@gmail.com>
2025-10-23 18:33:03 +0100

    9304

--- src/WallpaperSlideshow.tsx
+++ src/WallpaperSlideshow.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react'
 
 // Import wallpaper images
-import wallpaper1 from './assets/wallpaper1.jpeg'
-import wallpaper2 from './assets/wallpaper2.jpeg'
-import wallpaper3 from './assets/wallpaper3.jpeg'
-import wallpaper4 from './assets/wallpaper4.jpeg'
-import wallpaper5 from './assets/wallpaper5.jpeg'
+import wallpaper1 from './assets/wallpaper1.jpg'
+import wallpaper2 from './assets/wallpaper2.jpg'
+import wallpaper3 from './assets/wallpaper3.jpg'
+import wallpaper4 from './assets/wallpaper4.jpg'
+import wallpaper5 from './assets/wallpaper5.jpg'
 
 const WALLPAPERS = [
   wallpaper1,
@@ -21,17 +21,24 @@ interface WallpaperSlideshowProps {
 
 const WallpaperSlideshow: React.FC<WallpaperSlideshowProps> = ({ className = '' }) => {
   const [currentIndex, setCurrentIndex] = useState(0)
+  const [nextIndex, setNextIndex] = useState(1)
 
   useEffect(() => {
     // Set initial wallpaper
     document.documentElement.style.setProperty('--wallpaper-url', `url(${WALLPAPERS[0]})`)
+    document.documentElement.style.setProperty('--wallpaper-next-url', `url(${WALLPAPERS[1]})`)
     
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => {
-        const nextIndex = (prevIndex + 1) % WALLPAPERS.length
-        // Update CSS variable with new wallpaper
-        document.documentElement.style.setProperty('--wallpaper-url', `url(${WALLPAPERS[nextIndex]})`)
-        return nextIndex
+        const nextIdx = (prevIndex + 1) % WALLPAPERS.length
+        const nextNextIdx = (nextIdx + 1) % WALLPAPERS.length
+        
+        // Smooth crossfade transition
+        document.documentElement.style.setProperty('--wallpaper-url', `url(${WALLPAPERS[nextIdx]})`)
+        document.documentElement.style.setProperty('--wallpaper-next-url', `url(${WALLPAPERS[nextNextIdx]})`)
+        
+        setNextIndex(nextNextIdx)
+        return nextIdx
       })
     }, 15000) // 15 seconds
 
