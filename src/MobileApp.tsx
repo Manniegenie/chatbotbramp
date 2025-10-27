85dfa37acffe8c5ca5fb04d4bd569b8a779900f4
Chibuike Nwogbo <manuelwurld@gmail.com>
2025-10-27 12:42:19 +0100

    Fix Tawk.to script placement according to official guide
    - Moved Tawk.to script from footer to just before closing body tag
    - Follows Tawk.to official installation guide recommendations
    - Script now loads properly at the end of page rendering
    - Applied fix to both App.tsx and MobileApp.tsx
    - Widget should now appear correctly in bottom-right corner

--- src/MobileApp.tsx
+++ src/MobileApp.tsx
@@ -959,24 +959,24 @@ export default function MobileApp() {
           <a href="/terms" target="_blank" rel="noopener noreferrer">Terms</a>
         </div>
       </footer>
-
-      {/* Tawk.to Support Widget */}
-      <script
-        type="text/javascript"
-        dangerouslySetInnerHTML={{
-          __html: `
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/68ff552f1a60b619594aac17/1j8im9gmc';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-            })();
-          `
-        }}
-      />
     </div>
+
+    {/* Tawk.to Support Widget - Placed before closing body tag */}
+    <script
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+          (function(){
+          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+          s1.async=true;
+          s1.src='https://embed.tawk.to/68ff552f1a60b619594aac17/1j8im9gmc';
+          s1.charset='UTF-8';
+          s1.setAttribute('crossorigin','*');
+          s0.parentNode.insertBefore(s1,s0);
+          })();
+        `
+      }}
+    />
   )
 }
