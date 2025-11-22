bef9f8cdea4730ed4a3a17acd9e5de16ecabf352
Chibuike Nwogbo <manuelwurld@gmail.com>
2025-11-22 22:04:40 +0100

    4894948

--- src/MobileApp.tsx
+++ src/MobileApp.tsx
@@ -1237,7 +1237,7 @@ export default function MobileApp() {
           <footer className="mobile-footer">
             <div className="mobile-footer-links-bottom">
               <a href="https://drive.google.com/file/d/11qmXGhossotfF4MTfVaUPac-UjJgV42L/view?usp=drive_link" target="_blank" rel="noopener noreferrer">AML/CFT Policy</a>
-              <a href="https://drive.google.com/file/d/1brtkc1Tz28Lk3Xb7C0t3--wW7829Txxw/view?usp=drive_link" target="_blank" rel="noopener noreferrer">Privacy</a>
+              <a href="https://drive.google.com/file/d/1brtkc1Tz28Lk3Xb7C0t3--wW7829Txxw/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="footer-privacy-link">Privacy</a>
               <a href="https://www.instagram.com/chatbramp/" target="_blank" rel="noopener noreferrer">Instagram</a>
               <a href="https://www.youtube.com/@Chatbramp" target="_blank" rel="noopener noreferrer">YouTube</a>
               <a href="https://x.com/Chatbramp" target="_blank" rel="noopener noreferrer">Twitter</a>
